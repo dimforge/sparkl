eb7fa9f821b846bd29568711a56e7a749a21a5fe
Sébastien Crozet <developer@crozet.re>
2022-12-07 15:38:05 +0100

    Add publish script

---
+++ publish.sh
@@ -0,0 +1,29 @@
+#! /bin/bash
+
+tmp=$(mktemp -d)
+
+echo "$tmp"
+
+cp -r src "$tmp"/.
+cp -r src_core "$tmp"/.
+cp -r src_kernels "$tmp"/.
+cp -r resources "$tmp"/.
+cp -r LICENSE README.md "$tmp"/.
+
+### Publish the 2D version.
+sed 's#\.\./\.\./src_core#src_core#g' crates/sparkl2d-core/Cargo.toml > "$tmp"/Cargo.toml
+currdir=$(pwd)
+cd "$tmp" && cargo publish
+cd "$currdir" || exit
+
+sed 's#\.\./\.\./src_kernels#src_kernels#g' crates/sparkl2d-kernels/Cargo.toml > "$tmp"/Cargo.toml
+currdir=$(pwd)
+cd "$tmp" && cargo publish
+cd "$currdir" || exit
+
+sed 's#\.\./\.\./src#src#g' crates/sparkl2d/Cargo.toml > "$tmp"/Cargo.toml
+currdir=$(pwd)
+cd "$tmp" && cargo publish
+cd "$currdir" || exit
+
+rm -rf "$tmp"
