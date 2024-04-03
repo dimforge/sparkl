8583a1f2590deefeb915e4e150948c8cc072d965
Vollkornaffe <Vollkornaffe@gmail.com>
2024-04-03 15:59:54 +0200

    feat: Allow for downcasting plastic model
    As here: https://stackoverflow.com/questions/76222743/upcasting-an-arcdyn-trait-to-an-arcdyn-any
    There is probably a cleaner solution, such as using the as-any crate?

--- src/dynamics/models/plastic_model.rs
+++ src/dynamics/models/plastic_model.rs
@@ -1,7 +1,20 @@
+use std::any::Any;
+use std::sync::Arc;
+
 use crate::core::prelude::CorePlasticModel;
 use crate::dynamics::Particle;
 
-pub trait PlasticModel: Send + Sync {
+pub trait AsAnyArc {
+    fn as_any_arc(self: Arc<Self>) -> Arc<dyn Any>;
+}
+
+impl<T: 'static> AsAnyArc for T {
+    fn as_any_arc(self: Arc<Self>) -> Arc<dyn Any> {
+        self
+    }
+}
+
+pub trait PlasticModel: Send + Sync + AsAnyArc {
     // TODO: this should just take the deformation gradient as input and ouptut
     //       the projected deformation gradient.
     fn update_particle(&self, particle: &mut Particle);
